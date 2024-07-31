05d0399452ea63f716a9a02885d26633a6b52d5e
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-08-01 00:51:06 +0200

    proyecto final

---
+++ ProyectoFinal/carts-service/src/main/java/com/todocodeacademy/shop/carts_service/CartsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.shop.carts_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartsServiceApplication.class, args);
+	}
+
+}
